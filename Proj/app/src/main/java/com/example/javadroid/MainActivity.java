b850e815d0d356e14de1834a39455aef8496400b
rgl37 <lawson.rain@gmail.com>
2023-11-15 16:01:19 -0600

    Updated Labels/Title

--- Proj/app/src/main/java/com/example/javadroid/MainActivity.java
+++ Proj/app/src/main/java/com/example/javadroid/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("JavaDroid");
     }
 
     public void Disable (View currentView) {
